e50c1f864d52d79d388a2d5891e839b3cc1def31
marlonmp <76027579+marlonmp@users.noreply.github.com>
2026-08-16 00:43:29 -0500

    feat(vault): getItemIdx method added, comment documentations added and soft lock method implemented

--- src/vault/vault.go
+++ src/vault/vault.go
@@ -25,6 +25,16 @@ func generateVaultLabel(id uuid.UUID) []byte {
 	return label.Bytes()
 }
 
+type itemContent struct {
+	Password   []byte `json:"password"`
+	OTPAuthURI []byte `json:"otpauth_uri"`
+}
+
+func (ic *itemContent) erase() {
+	subtle.XORBytes(ic.Password, ic.Password, ic.Password)
+	subtle.XORBytes(ic.OTPAuthURI, ic.OTPAuthURI, ic.OTPAuthURI)
+}
+
 type encryptedItem struct {
 	ID        uuid.UUID `json:"id"`
 	Title     string    `json:"titile"`
@@ -36,14 +46,8 @@ type encryptedItem struct {
 	UpdatedAt time.Time `json:"updated_at"`
 }
 
-type itemContent struct {
-	Password   []byte `json:"password"`
-	OTPAuthURI []byte `json:"otpauth_uri"`
-}
-
-func (ic *itemContent) erase() {
-	subtle.XORBytes(ic.Password, ic.Password, ic.Password)
-	subtle.XORBytes(ic.OTPAuthURI, ic.OTPAuthURI, ic.OTPAuthURI)
+func (ei *encryptedItem) erase() {
+	ei = new(encryptedItem)
 }
 
 func (ei encryptedItem) copy() encryptedItem {
@@ -75,15 +79,15 @@ func (ei encryptedItem) unlock(key []byte) (*Item, error) {
 	tags := make([]string, len(ei.Tags))
 	copy(tags, ei.Tags)
 	item := Item{
-		ID: ei.ID,
-		Title: ei.Title,
-		URL: ei.URL,
-		User: ei.User,
-		Tags: tags,
-		Password: content.Password,
+		ID:         ei.ID,
+		Title:      ei.Title,
+		URL:        ei.URL,
+		User:       ei.User,
+		Tags:       tags,
+		Password:   content.Password,
 		OTPAuthURI: content.OTPAuthURI,
-		CreatedAt: ei.CreatedAt,
-		UpdatedAt: ei.UpdatedAt,
+		CreatedAt:  ei.CreatedAt,
+		UpdatedAt:  ei.UpdatedAt,
 	}
 	content.erase()
 	return &item, nil
@@ -101,8 +105,8 @@ type Item struct {
 	UpdatedAt  time.Time
 }
 
-// securely erase item password and otpauth uri from memory
-func (i *Item) eraseContent() {
+// securely erase the item password and otpauth uri from memory
+func (i *Item) Erase() {
 	subtle.XORBytes(i.Password, i.Password, i.Password)
 	subtle.XORBytes(i.OTPAuthURI, i.OTPAuthURI, i.OTPAuthURI)
 }
@@ -139,7 +143,7 @@ func (i *Item) lock(key []byte) (encryptedItem, error) {
 	}
 	// erase password and otp auth uri from memory
 	content.erase()
-	i.eraseContent()
+	i.Erase()
 	return encItem, nil
 }
 
@@ -171,7 +175,7 @@ func (ev encryptedVault) Unlock(priv *rsa.PrivateKey) (*vault, error) {
 	items := make([]encryptedItem, 0)
 	err = json.Unmarshal(jsonItems, &items)
 	if err != nil {
-		return nil, nil
+		return nil, err
 	}
 	// build vault
 	vault := &vault{
@@ -207,7 +211,35 @@ func New(title string) *vault {
 	}
 }
 
-// creates a enc items deep copy
+// if an item exists return the item index else returns
+// `NoVaultItemFoundError` error
+func (v *vault) getItemIdx(id uuid.UUID) (int, error) {
+	// if there are no items, the item does not exist
+	if len(v.encItems) == 0 {
+		return -1, NoVaultItemFoundError
+	}
+	// if there is one item and the ids are different, the item does not exist
+	if len(v.encItems) == 1 && v.encItems[0].ID != id {
+		return -1, NoVaultItemFoundError
+	}
+	// if there is one item and the ids are equal, the item exist
+	if len(v.encItems) == 1 && v.encItems[0].ID == id {
+		return 0, nil
+	}
+	idx := -1
+	for i, ei := range v.encItems {
+		if ei.ID == id {
+			idx = i
+			break
+		}
+	}
+	if idx == -1 {
+		return -1, NoVaultItemFoundError
+	}
+	return idx, nil
+}
+
+// creates a enc items list deep copy
 func (v *vault) GetItems() []encryptedItem {
 	if len(v.encItems) == 0 {
 		return make([]encryptedItem, 0)
@@ -219,7 +251,8 @@ func (v *vault) GetItems() []encryptedItem {
 	return encItems
 }
 
-func (v *vault) AddItem(item *Item) error {
+// appends a new locked item in the enc items list
+func (v *vault) AddItem(item *Item) (uuid.UUID, error) {
 	// ensure default data
 	item.ID = uuid.New()
 	item.CreatedAt = time.Now()
@@ -227,83 +260,48 @@ func (v *vault) AddItem(item *Item) error {
 	// encrypt item
 	encItem, err := item.lock(v.key)
 	if err != nil {
-		return err
+		return uuid.Nil, err
 	}
-	defer item.eraseContent()
+	defer item.Erase()
 	if len(v.encItems) == 0 {
 		v.encItems = make([]encryptedItem, 1)
 		v.encItems[0] = encItem
-		return nil
+		return item.ID, nil
 	}
 	// add item
 	v.encItems = append(v.encItems, encItem)
 	// securely erase item content
-	return nil
+	return item.ID, nil
 }
 
+// if there is an item with the given id, updates the enc item with the
+// locked givet item
 func (v *vault) UpdateItem(item *Item) error {
-	// zero items check
-	if len(v.encItems) == 0 {
-		return NoVaultItemFoundError
+	idx, err := v.getItemIdx(item.ID)
+	if err != nil {
+		return err
 	}
-	// one item check
-	if len(v.encItems) == 1 && v.encItems[0].ID != item.ID {
-		return NoVaultItemFoundError
-	}
+	// manualy updated at
 	item.UpdatedAt = time.Now()
 	encItem, err := item.lock(v.key)
 	if err != nil {
-		return nil
+		return err
 	}
-	defer item.eraseContent()
-	if len(v.encItems) == 1 && v.encItems[0].ID == item.ID {
-		v.encItems[0] = encItem
-		return nil
-	}
-	// find in items list
-	idx := new(uint32)
-	for i, ei := range v.encItems {
-		if ei.ID == item.ID {
-			n := uint32(i)
-			idx = &n
-			break
-		}
-	}
-	if idx == nil {
-		return NoVaultItemFoundError
-	}
-	v.encItems[*idx] = encItem
+	defer item.Erase()
+	v.encItems[idx] = encItem
 	return nil
 }
 
+// if there is a item with the given id, creates a new list of enc items
+// without the item found
 func (v *vault) DeleteItem(id uuid.UUID) error {
-	// zero items check
-	if len(v.encItems) == 0 {
-		return NoVaultItemFoundError
-	}
-	// one item check
-	if len(v.encItems) == 1 && v.encItems[0].ID != id {
-		return NoVaultItemFoundError
-	}
-	if len(v.encItems) == 1 && v.encItems[0].ID == id {
-		v.encItems = make([]encryptedItem, 0)
-		return nil
-	}
-	// find in items list
-	idx := new(uint32)
-	for i, ei := range v.encItems {
-		if ei.ID == id {
-			n := uint32(i)
-			idx = &n
-			break
-		}
-	}
-	if idx == nil {
-		return NoVaultItemFoundError
+	idx, err := v.getItemIdx(id)
+	if err != nil {
+		return err
 	}
 	encItems := make([]encryptedItem, len(v.encItems)-1)
 	for i := range len(encItems) {
-		if uint32(i) == *idx {
+		if i == idx {
 			continue
 		}
 		encItems[i] = v.encItems[i]
@@ -312,56 +310,44 @@ func (v *vault) DeleteItem(id uuid.UUID) error {
 	return nil
 }
 
+// if there is an item with the given id, returns the unlocked item
+//
+// WARN: is recommendend to use the `Erase` method when the item wont be
+// used anymore
 func (v *vault) UnlockItem(id uuid.UUID) (*Item, error) {
-	if len(v.encItems) == 0 {
-		return nil, NoVaultItemFoundError
+	idx, err := v.getItemIdx(id)
+	if err != nil {
+		return nil, err
 	}
-	if len(v.encItems) == 1 && v.encItems[0].ID != id {
-		return nil, NoVaultItemFoundError
-	}
-	if len(v.encItems) == 1 && v.encItems[0].ID == id {
-		return v.encItems[0].unlock(v.key)
-	}
-	// find in items list
-	idx := new(uint32)
-	for i, ei := range v.encItems {
-		if ei.ID == id {
-			n := uint32(i)
-			idx = &n
-			break
-		}
-	}
-	if idx == nil {
-		return nil, NoVaultItemFoundError
-	}
-	return v.encItems[*idx].unlock(v.key)
+	return v.encItems[idx].unlock(v.key)
 }
 
 // This method securely erases the key from memory
-func (v *vault) eraseKey() {
+func (v *vault) erase() {
 	subtle.XORBytes(v.key, v.key, v.key)
 }
 
-// This method encrypt the vault items and the vault key and returns an encrypted vault
-func (v *vault) Lock(pub *rsa.PublicKey) (*encryptedVault, error) {
+// This method encrypt the vault items with the vault key and encrypt
+// the vault key with the gived public key returning an encrypted vault
+func (v *vault) SoftLock(pub *rsa.PublicKey) (encryptedVault, error) {
 	// marshal encrypted items list
 	jsonEncItems, err := json.Marshal(v.encItems)
 	if err != nil {
-		return nil, err
+		return encryptedVault{}, err
 	}
 	// encrypt encrypted items list
 	cipherEncItems, err := EncryptAESGCM(jsonEncItems, v.key)
 	if err != nil {
-		return nil, err
+		return encryptedVault{}, err
 	}
 	// generate key encryption label
 	label := generateVaultLabel(v.id)
 	// encrypt the encryption key with the public key
 	cipherKey, err := rsa.EncryptOAEP(sha256.New(), rand.Reader, pub, v.key, label)
 	if err != nil {
-		return nil, err
+		return encryptedVault{}, err
 	}
-	encVault := &encryptedVault{
+	encVault := encryptedVault{
 		ID:        v.id,
 		Title:     v.title,
 		Key:       cipherKey,
@@ -369,7 +355,21 @@ func (v *vault) Lock(pub *rsa.PublicKey) (*encryptedVault, error) {
 		CreatedAt: v.createdAt,
 		UpdatedAt: v.updatedAt,
 	}
+	return encVault, nil
+}
+
+// This method encrypt the vault items with the vault key and encrypt
+// the vault key with the gived public key returning an encrypted vault
+// and erasing secitive data from memory.
+//
+// NOTE: once the vault is locked, the vault key is erased from
+// memory the vault cannot be used anymore
+func (v *vault) Lock(pub *rsa.PublicKey) (encryptedVault, error) {
+	encVault, err := v.SoftLock(pub)
+	if err != nil {
+		return encryptedVault{}, err
+	}
 	// securely erase the key from memory
-	v.eraseKey()
+	v.erase()
 	return encVault, nil
 }
